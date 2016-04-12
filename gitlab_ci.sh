dadd85608fc56443ce76e05fe6c83c4e9d3d302a
kartofeler <andrzejdaniel.szm@gmail.com>
2016-04-12 11:28:31 +0200

    Update gitlab_ci.sh

--- gitlab_ci.sh
+++ gitlab_ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -sSL https://get.docker.com/ | sh
-https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
 apt-get install gitlab-ci-multi-runner -y
